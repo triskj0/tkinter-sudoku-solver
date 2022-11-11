cb63f3154ab5441cd707a857a80d92c80c8bf0b4
triskj0 <108220065+triskj0@users.noreply.github.com>
2022-11-11 18:35:33 +0100

    fixed a bug
    rewrote the entire code

--- check_board_validity.py
+++ check_board_validity.py
@@ -1,33 +1,18 @@
 def validate_board(board):
-	# check rows
-	for r in range(9):
-		row_vals = board[r]		
-		row_vals = list(filter(lambda x: x != 0, row_vals))
+	rows = [set() for x in range(9)]
+	columns = [set() for x in range(9)]
+	squares = [[set() for x in range(3)] for y in range(3)]
+	
+	for x in range(9):
+		for y in range(9):
+			cell_value = board[x][y]
+			if cell_value == 0:
+				continue
+			if cell_value in rows[x] or cell_value in columns[y] or cell_value in squares[x//3][y//3]:
+				return False
 
-		if len(set(row_vals)) != len(row_vals):
-			return False	
-
-	# check columns
-	for c in range(9):
-		col_vals = [board[r][c] for r in range(9)]
-		col_vals = list(filter(lambda x: x != 0, col_vals))
-
-		if len(set(col_vals)) != len(col_vals):
-			return False
-
-	# check squares
-	for s in range(9):
-		square_vals = []
-		row_start = s // 3 * 3
-		col_start = s // 3 * 3
-
-		for r in range(row_start, row_start+3):
-			for c in range(col_start, col_start+3):
-				square_vals.append(board[r][c])
-
-		square_vals = list(filter(lambda x: x != 0, square_vals))
-
-		if len(set(square_vals)) != len(square_vals):
-			return False
-
+			rows[x].add(cell_value)
+			columns[y].add(cell_value)
+			squares[x//3][y//3].add(cell_value)
+	
 	return True
